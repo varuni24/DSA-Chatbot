feda0f7b6a42548b356d42ed4c915395b9118f05
Varuni Gupta <85125668+varuni24@users.noreply.github.com>
2024-06-14 09:40:09 +0400

    Delete download_nltk.py

--- download_nltk.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.tokenize import word_tokenize
-
-nltk.download('punkt')  # Download necessary NLTK data (if not downloaded already)
-
-text = "NLTK is awesome!"
-tokens = word_tokenize(text)
-print(tokens)
